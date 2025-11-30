dd8bc49e59a1737ad1a10b2eaa36b0e972537f42
Bakaluke <icaromsilva15@gmail.com>
2025-11-30 18:10:52 -0300

    Updates.

--- api/routes/web.php
+++ api/routes/web.php
@@ -3,18 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\QuoteController;
 use App\Http\Controllers\Api\ProductionOrderController;
-use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/instalar-sgi-forca-bruta', function () {
-    try {
-        Artisan::call('optimize:clear');
-        Artisan::call('migrate:fresh --seed --force');        
-        return "<h1>SUCESSO!</h1><p>Banco de dados recriado e populado.</p><pre>" . Artisan::output() . "</pre>";
-    } catch (\Exception $e) {
-        return "<h1>ERRO!</h1><p>" . $e->getMessage() . "</p>";
-    }
 });
